e5deb28f864c3f9412c89dfdd0fb9fd6df5c0dab
twizzlynn <twizzlynn@gmail.com>
2015-06-06 01:41:04 -0500

    jade

--- index.js
+++ index.js
@@ -2,15 +2,12 @@ var express = require('express');
 var app = express();
 var cool = require('cool-ascii-faces');
 var pg = require('pg');
+app.set('view engine', 'jade');
 
 app.set('port', (process.env.PORT || 5000));
 
-app.get('/', function(request, response) {
-    var result = ''
-    var times = process.env.TIMES || 5
-    for (i=0; i < times; i++)
-    result += cool();
-    response.send(result);
+app.get('/', function(req, res){
+    res.render('index', { title: 'My Site' });
 });
 
 app.listen(app.get('port'), function() {
