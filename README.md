8145c3429170018b16c5441532daa936e84170dc
twizzlynn <twizzlynn@gmail.com>
2015-06-06 05:22:19 -0500

    latest

--- README.md
+++ README.md
@@ -4,9 +4,11 @@ global hack 4
 tools for publishers
 - right now can just promote and get feedback
 
-make native content - new content page
-- has user interaction for benefit
+make native content - new content page - separate heroku instance
+- TODO: brainstorm: user interaction for publisher benefit
 
-access content in single page - display
+access content in single page - TODO: strip away other stuff, make full screen
+$(".top-toolbar-cntnr").remove()
 
 embed link in external site
+TODO make pretty; include styles
--- index.js
+++ index.js
@@ -1,18 +1,32 @@
 var express = require('express');
 var app = express();
-var cool = require('cool-ascii-faces');
 var pg = require('pg');
-var path = require("path");
+var request = require('request');
+var bodyParser = require('body-parser');
+var multer = require('multer');
 
 app.set('port', (process.env.PORT || 5000));
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(multer()); // for parsing multipart/form-data
 
-app.use('/public', express.static('public'));
 app.use('/node_modules', express.static('node_modules'));
 app.use('/js', express.static('js'));
 app.use('/css', express.static('css'));
 
+function log(msg) {
+    console.log(msg);
+}
+
+var APP_ID = 7740523506827330;
+var APP_SECRET = "l6l8QHRBUVJyn+1NqjZq7p4uERt+gTc17a7KA6fIV/tNwfTjxkrGTfc3np909WnDCwoQ4Y4p90Q69vWRcOv2fg==";
 
 app.get('/', function(req, res){
+    request('http://api.globalhack4.test.lockerdome.com/app_create_content?{"app_id":7740523506827330,"app_secret":"l6l8QHRBUVJyn+1NqjZq7p4uERt+gTc17a7KA6fIV/tNwfTjxkrGTfc3np909WnDCwoQ4Y4p90Q69vWRcOv2fg==","app_data":{"fun":"times"},"name":"Some App Content","text":"Short description of your content"}', function (error, response, body) {
+        if (!error && res.statusCode == 200) {
+            console.log(body); // Show the HTML for the Google homepage.
+        }
+    });
     res.sendFile(__dirname + '/index.html');
 });
 
@@ -20,14 +34,19 @@ app.listen(app.get('port'), function() {
     console.log("Node app is running on port:" + app.get('port'))
 })
 
-app.get('/db', function (request, response) {
+app.get('/db', function (req, res) {
     pg.connect(process.env.DATABASE_URL, function(err, client, done) {
         client.query('SELECT * FROM test_table', function(err, result) {
             done();
             if (err)
-            { console.error(err); response.send("Error " + err); }
+            { console.error(err); res.send("Error " + err); }
             else
-            { response.send(result.rows); }
+            { res.send(result.rows); }
         });
     });
-})
+});
+
+app.get('/hook', function (req, res) {
+    console.log(req.body);
+    res.json(req.body);
+});
