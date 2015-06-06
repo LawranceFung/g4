56c1017a0c702c3d632d94011cd5c2453ffd92da
twizzlynn <twizzlynn@gmail.com>
2015-06-06 01:08:13 -0500

    latest

--- index.js
+++ index.js
@@ -6,26 +6,25 @@ var pg = require('pg');
 app.set('port', (process.env.PORT || 5000));
 
 app.get('/', function(request, response) {
-  var result = ''
-  var times = process.env.TIMES || 5
-  for (i=0; i < times; i++)
+    var result = ''
+    var times = process.env.TIMES || 5
+    for (i=0; i < times; i++)
     result += cool();
-  response.send(result);
+    response.send(result);
 });
 
 app.listen(app.get('port'), function() {
     console.log("Node app is running on port:" + app.get('port'))
 })
 
-
 app.get('/db', function (request, response) {
-  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM test_table', function(err, result) {
-      done();
-      if (err)
-       { console.error(err); response.send("Error " + err); }
-      else
-       { response.send(result.rows); }
+    pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+        client.query('SELECT * FROM test_table', function(err, result) {
+            done();
+            if (err)
+            { console.error(err); response.send("Error " + err); }
+            else
+            { response.send(result.rows); }
+        });
     });
-  });
 })
